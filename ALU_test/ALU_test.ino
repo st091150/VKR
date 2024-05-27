ad47232f8cbe298b502ffe7cef8064c9fd7226df
Артём Мещерин <72318993+st091150@users.noreply.github.com>
2024-05-27 07:41:18 -0800

    Update ALU_test.ino

--- ALU_test/ALU_test.ino
+++ ALU_test/ALU_test.ino
@@ -1,62 +1,66 @@
-const byte numOutANumPins = 4;
-const byte numOutBNumPins = 4;
-const byte numOutOperPins = 3;
-const byte numInNumPins = 4;
+const byte numOutANumPins = 4; // количество выходных битов 1-ого операнда 
+const byte numOutBNumPins = 4; // количество выходных битов 2-ого операнда
+const byte numOutOperPins = 3; // количество выходных управляющих битов
+const byte numInNumPins = 4; // количество входящих битов
 
-byte numOutAPins[] = {2, 3, 4, 5};
-byte numOutBPins[] = {A3, A2, A1, A0};
+byte numOutAPins[] = {2, 3, 4, 5}; // пины для подачи 1 операнда
+byte numOutBPins[] = {A3, A2, A1, A0}; // пины для подачи 2 операнда
 
-byte outOperPins[] = {12, 11, 10};
-byte inPins[] = {6, 7, 8, 9};
+byte outOperPins[] = {12, 11, 10}; // пины для подачи выходных управляющих битов
+byte inPins[] = {6, 7, 8, 9}; // входные пины для результата
 
-
+// поддерживаемые операции
 String operations[] = {"OR", "NAND", "NOR", "AND", "ADD", "SUB"};
+
+//коды поддерживаемых операций 
 
 byte operationsBits[][3] = {{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
                             {1, 0, 0}, {1, 0, 1}};
 
-
 struct aluTest{
 
-  int A;
-  int B;
+  int A; // 1 операнд 
+  int B; // 2 операнд
 
-  byte operationIdx;
-  byte operation[3];
+  byte operationIdx; // индекс в массиве поддерживаемых операций
+  byte operation[3]; // код текущей операции
 
-  int result;
-  int computerResult;
+  int result; // переменная для подсчета результата
+  int computerResult; // переменная для хранения результата работы стенда
 
   
   void test(int numOfTests, byte* outNumAPins, byte* outNumBPins, byte* outOperPins, byte* inPins){
-    int successTest = 0;
-    int failTest = 0;
+    int successTest = 0; // количество пройденных тестов
+    int failTest = 0; // количество провальных тестов
     for(int i = 0; i < numOfTests; ++i){
       result = 0;
       computerResult = 0;
-      A = 8;
-      B = 5;
+      A = random(16); // получение случайного 1-ого операнда в диапазоне [0,16)
+      B = random(16); // получение случайного 2-ого операнда в диапазоне [0,16)
 
-      printA();
-      printB();
+      printA(); // вывод 1-ого операнда
+      printB(); // вывод 2-ого операнда
 
-      operationIdx = 3;
+      operationIdx = random(6); // получение случайного индекса в массиве операций
+	// получение кода операции
       memcpy(operation, operationsBits[operationIdx], 3 * sizeof(byte));
 
-      
+      // подача на плату 1-ого операнда
       for(int i = 0; i < numOutANumPins ; ++i){
         digitalWrite(numOutAPins[i],  bitRead(A, i));
       }
+	// подача на плату 2-ого опереанда
         for(int i = 0; i < numOutBNumPins ; ++i){
         digitalWrite(numOutBPins[i],  bitRead(B, i));
       }
+	// подача управляющих сигналов на плату
         for(int i = 0; i < numOutOperPins ; ++i){
         digitalWrite(outOperPins[i], operation[numOutOperPins - 1 - i]);
       }
-
+	// нахождение истинного результата
       switch(operationIdx){
         case 0:
-          result = A | B;
+          result = (A | B) & 15;
           break;
         case 1:
           result = ~(A & B) & 15;
@@ -65,7 +69,7 @@ struct aluTest{
           result = ~(A | B) & 15;
           break;
         case 3:
-          result = A & B;
+          result = ~(A & B) & 15;
           break;
         case 4:
           result = (A + B) & 15;
@@ -74,25 +78,25 @@ struct aluTest{
           result = (A - B) & 15;
           break;
       }
-      printOperBits();
-      printResult();
-      printComputerResult();
+      printOperBits(); // вывод кода операции
+      printResult(); // вывод истинного результата
+      printComputerResult(); // вывод результата полученного с АЛУ
 
+	    // проверка правильности результата
       if(result == computerResult){
         successTest++;
       }else{
-        Serial.print("#######################");
         failTest++;
       }
 
-
     }
+    // вывод результатов тестирования
     Serial.print("SUCCESS Test: ");
     Serial.print(successTest);
     Serial.print("/ ");
     Serial.println(numOfTests);
   }
-
+  // функция для вывода 1-ого операнда
   void printA(){
     Serial.print("A: ");
     Serial.println(A);
@@ -102,7 +106,7 @@ struct aluTest{
     }
     Serial.println();
   }
-
+    // функция для вывода 1-ого операнда
     void printB(){
     Serial.print("B: ");
     Serial.println(B);
@@ -112,7 +116,7 @@ struct aluTest{
     }
     Serial.println();
   }
-
+  // функция для вывода истинного результата
   void printResult(){
     Serial.print("Result: ");
     Serial.println(result);
@@ -122,7 +126,7 @@ struct aluTest{
     }
     Serial.println();
   }
-
+  // функция вывода результата работы АЛУ
   void printComputerResult(){
     Serial.print("ComputerResult: ");
     for(int i = 0; i < numInNumPins; ++i){
@@ -136,7 +140,8 @@ struct aluTest{
     Serial.println();
   }
 
-  void printOperBits(){
+// вывод кода текущей операции
+void printOperBits(){
     Serial.print("Operation: ");
     Serial.println(operations[operationIdx]);
     Serial.print("OperationBits: ");
@@ -153,25 +158,22 @@ aluTest cTest;
 void setup() {
   Serial.begin(9600);
 
+  // установка выходных пинов для 1-ого операнда в режим OUTPUT
   for(int i = 0; i < numOutANumPins; ++i){
     pinMode(numOutAPins[i], OUTPUT);
   }
+  // установка выходных пинов для 2-ого операнда в режим OUTPUT
   for(int i = 0; i < numOutBNumPins; ++i){
     pinMode(numOutBPins[i], OUTPUT);
   }
-    for(int i = 0; i < numOutOperPins; ++i){
+  // установка выходных пинов для управляющих сигналов в режим OUTPUT
+  for(int i = 0; i < numOutOperPins; ++i){
     pinMode(outOperPins[i], OUTPUT);
   }
-
+  // установка входных пинов для получения результата работы АЛУ
   for(int i = 0; i < numInNumPins; ++i){
     pinMode(inPins[i], INPUT);
   }
-  
-  cTest.test(10, numOutAPins, numOutBPins, outOperPins, inPins);
-
+  // запуска тестирования
+  cTest.test(1000, numOutAPins, numOutBPins, outOperPins, inPins);
 }
-
-void loop() {
-
-}
-
